ccc867255e28cd2c4f40e4314f1e58132defe8ee
Giannina <giannisedler@hotmail.com>
2022-09-21 16:40:19 -0300

    01 Node hmw HECHO

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -1,16 +1,25 @@
 const commands = require('./commands');
 
+const done = function(output){
+  process.stdout.write(output);
+  process.stdout.write('\nprompt > ');
+}
+
 // Output un prompt
 process.stdout.write('prompt > ');
 // El evento stdin 'data' se dispara cuando el user escribe una línea
+// date ---> cmd = 'date' ---> commands[cmd]() ---> commands['date'] 
+// echo hola mundo ---> hola mundo
+// "echo hola mundo".split(' ')-->["echo", "hola", "mundo"].shift()--> "echo"-->["hola", "mundo"]
+
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remueve la nueva línea
-  if(cmd === 'date') {
-    process.stdout.write(Date());  
-  }
-  if(cmd === 'pwd') {
-    // process.cwd()
-    process.stdout.write(process.cwd())
-  }
-  process.stdout.write('\nprompt > ');
+  var args = data.toString().trim().split(' '); // remueve la nueva línea
+  var cmd = args.shift()
+  if(commands.hasOwnProperty(cmd)) {
+    //process.stdout.write(Date()); 
+    commands[cmd](args, done) 
+  }else {
+    process.stdout.write('Command not found');
+  }  
+  // process.stdout.write('\nprompt > ');
 });
